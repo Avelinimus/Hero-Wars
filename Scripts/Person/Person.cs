083cb888419cc666797e0a5b188689e6870ebce5
Avelinimus <avel63146@gmail.com>
2020-11-26 17:10:57 +0200

    Image and class Person()

---
+++ Scripts/Person/Person.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person
+{
+    private static string[] genders = { "male", "female" };
+    private static string[] male_names = { };
+    private static string[] female_names = { };
+
+    List<string> gendersRange = new List<string>(genders);
+
+
+    public static string Info() 
+    {
+        return "";
+    }
+}
